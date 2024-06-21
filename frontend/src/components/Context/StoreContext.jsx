5bd610b5e503672a8fe1a2b867164efd54650828
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-06-21 17:44:30 +0530

    Update StoreContext.jsx

--- frontend/src/components/Context/StoreContext.jsx
+++ frontend/src/components/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
-  const url = "http://localhost:8000";
+  const url = "https://food-delivery-backend-whsh.onrender.com";
   const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
